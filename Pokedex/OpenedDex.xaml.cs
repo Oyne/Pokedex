8c7830bb72e57921e9ab6af36612b7de62057ddc
Andrey Litvinov <andreoyy.litvinovv@gmail.com>
2023-07-14 19:05:04 +0300

    Empty parts

--- Pokedex/OpenedDex.xaml.cs
+++ Pokedex/OpenedDex.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Pokedex
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for OpenedDex.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class OpenedDex : Window
     {
-        public MainWindow()
+        public OpenedDex()
         {
             InitializeComponent();
         }
